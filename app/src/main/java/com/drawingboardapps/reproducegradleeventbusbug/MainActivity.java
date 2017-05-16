86325b1e09c3ea828c130c755e4386ca93a5c643
Zachary Nwabudike <zach@rokmobile.com>
2017-05-15 17:37:14 -0700

    first commit (kinda)

---
+++ app/src/main/java/com/drawingboardapps/reproducegradleeventbusbug/MainActivity.java
@@ -0,0 +1,27 @@
+package com.drawingboardapps.reproducegradleeventbusbug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        EventBus.getDefault().unregister(this);
+    }
+}
